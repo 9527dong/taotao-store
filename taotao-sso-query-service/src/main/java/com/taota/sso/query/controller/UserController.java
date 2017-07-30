d289f14f54260ad1ca526d87b9211c6a8c626500
Wang Dong <952764934@qq.com>
2017-07-30 23:41:15 +0800

    添加dubbo构建的登录模块

---
+++ taotao-sso-query-service/src/main/java/com/taota/sso/query/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.taota.sso.query.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.taotao.sso.query.api.UserQueryService;
+import com.taotao.sso.query.bean.User;
+
+@RequestMapping("user")
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserQueryService userQueryService;
+
+    /**
+     * 根据token查询用户信息
+     * 
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = "{token}", method = RequestMethod.GET)
+    public ResponseEntity<User> queryUserByToken(@PathVariable("token") String token) {
+        try {
+            User user = this.userQueryService.queryUserByToken(token);
+            if (null == user) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+}
